94e53fad5f65b570e24479c0c6073f3820e0d081
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-05-01 16:33:08 +0200

    added gameloop and counter

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
 import Cell from './cell';
 
+
+const vendors = ['webkit', 'moz'];
+for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+}
+const startTime = (new Date()).getTime();
+let currentTime = 0;
 const canvas = document.getElementById('canvas');
 
 const cw = canvas.width;
@@ -13,6 +21,25 @@ const numberOfCells = 100;
 const numberOfCellsInRow = 10;
 let ctx = null;
 
+function gameLoop() {
+    window.requestAnimationFrame(gameLoop);
+
+    currentTime = (new Date()).getTime();
+
+    const numberOfSecondsPassed = ((currentTime - startTime) / 1000);
+
+    ctx.clearRect(0, 0, 300, 50);
+    ctx.font = '30px Verdana';
+    // Create gradient
+    const gradient = ctx.createLinearGradient(0, 0, cw, 0);
+    gradient.addColorStop('0', 'magenta');
+    gradient.addColorStop('0.5', 'blue');
+    gradient.addColorStop('1.0', 'red');
+    // Fill with gradient
+    ctx.fillStyle = gradient;
+    ctx.fillText(numberOfSecondsPassed.toFixed(1), 10, 40);
+
+}
 
 canvas.addEventListener('click', click);
 
@@ -50,4 +77,6 @@ if (typeof (canvas.getContext) !== undefined) {
             counterX = 0;
         }
     }
+
+    gameLoop();
 }
