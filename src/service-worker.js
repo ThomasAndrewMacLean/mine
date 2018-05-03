5e9c0c931f48c1b6130b9a7928f369c15d6b6335
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-05-03 09:46:33 +0200

    test

--- src/service-worker.js
+++ src/service-worker.js
@@ -19,11 +19,11 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-    'index.html',
+
     './', // Alias for index.html
     // 'styles.css',
     // '../../styles/main.css',
-    'bundle.js'
+
 ];
 
 // The install handler takes care of precaching the resources we always need.
