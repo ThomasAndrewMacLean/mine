de74f65bd5e78d6683641c2bab0d9aa079d7cabc
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-05-10 16:02:09 +0200

    show loader on loading scores

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -2,6 +2,9 @@
 <div class="section">
   <div class="container">
       <div class="title">High Scores</div>
+      <div class="center">
+      <button class="button is-loading is-large is-primary is-outlined" v-if="isLoading"></button>
+      </div>
     <div class="notification" v-for="(s,i) in scores" :key="s.id">
      {{i+1}}. {{s.name}} : <strong>{{Math.round(s.score * 100) / 100}}</strong> points
     </div>
@@ -14,7 +17,8 @@ export default {
   name: "Game",
   data() {
     return {
-      scores: []
+      scores: [],
+      isLoading: true
     };
   },
   methods: {
@@ -22,11 +26,12 @@ export default {
   },
   mounted() {
     fetch(
-      'https://mjbwnqaj06.execute-api.eu-west-1.amazonaws.com/prod/gethighscores?limit=5'
+      "https://mjbwnqaj06.execute-api.eu-west-1.amazonaws.com/prod/gethighscores?limit=5"
     )
       .then(res => res.json())
       .then(r => {
         this.scores = r.filter(i => i.name).sort((a, b) => a.score < b.score);
+        this.isLoading = false;
       });
   }
 };
